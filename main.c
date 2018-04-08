30222ea60ad4a492804edeabe275338de850be0c
João Paulo Moraes <joaoded@hotmail.com>
2018-04-08 20:45:15 -0300

    Deleted unnecessary code

--- main.c
+++ main.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-int matricula;
-char nome[30];
-int idade;
-struct Aluno *proximo;
-struct Aluno *anterior;
-}Aluno;
 int callFunction(char *string, char letra);
 int funcao01RecusivaCauda(char *string, char letra,int soma);
 int funcao01Recursiva(char *string, char letra);
-
 
 int main(void) {
 	setbuf(stdout,NULL);
@@ -23,24 +15,6 @@ int main(void) {
 	return 0;
 }
 
-Aluno* inserir(Aluno *lista){
-	Aluno *novo;
-	novo = (Aluno*) malloc(sizeof(Aluno));
-	novo->proximo = NULL;
-	novo->anterior = NULL;
-	printf("Digite a matricula: ");
-	scanf("%d", novo->matricula);
-	printf("Digite o nome: ");
-	gets(novo->nome);
-
-	printf("Digite a idade: ");
-	scanf("%d", novo->idade);
-
-	novo->proximo = lista;
-	lista->anterior = novo;
-	return novo;
-
-}
 int callFunction(char *string, char letra){
 	return funcao01RecusivaCauda(string,letra, 0);
 }
